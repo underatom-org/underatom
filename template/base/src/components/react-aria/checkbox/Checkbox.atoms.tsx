40a8722ade1c6d4d7096a84a3e143f3e9e24df03
giuliano-underatom <giuliano@underatom.com>
2024-09-18 17:30:38 +0300

    checkbox implementation

---
+++ template/base/src/components/react-aria/checkbox/Checkbox.atoms.tsx
@@ -0,0 +1,53 @@
+import { CheckboxButtonLayout, CheckboxLayout } from "../../../styles/checkbox";
+import {
+  UCheckboxAsterisk,
+  UCheckboxDescription,
+  UCheckboxButtonIndicator,
+  UCheckboxButtonProps,
+  UCheckboxButtonRoot,
+  UCheckboxLabel,
+  UCheckboxRoot,
+  UCheckboxRootProps,
+  useCheckboxInternalProvider,
+} from "./Checkbox.underatoms";
+
+export type CheckboxProps = Omit<
+  UCheckboxRootProps & {
+    label?: string;
+    description?: string;
+  },
+  "children"
+>;
+
+export const Checkbox = ({ label, description, className, ...props }: CheckboxProps) => {
+  return (
+    <CheckboxLayout
+      renderRoot={(children, className) => (
+        <UCheckboxRoot {...props} className={className}>
+          {children}
+        </UCheckboxRoot>
+      )}
+      styleProps={{ className, checkboxProps: props }}
+      descriptionSlot={description && <UCheckboxDescription>{description}</UCheckboxDescription>}
+      asteriskSlot={props.isRequired && <UCheckboxAsterisk />}
+      labelSlot={label && <UCheckboxLabel>{label}</UCheckboxLabel>}
+      buttonSlot={<CheckboxButton />}
+    />
+  );
+};
+
+export type CheckboxButtonProps = Omit<UCheckboxButtonProps, "asChild" | "children">;
+export const CheckboxButton = (props: CheckboxButtonProps) => {
+  const checkboxProps = useCheckboxInternalProvider();
+  return (
+    <CheckboxButtonLayout
+      renderRoot={(children, className) => (
+        <UCheckboxButtonRoot {...props} className={className}>
+          {children}
+        </UCheckboxButtonRoot>
+      )}
+      styleProps={{ className: props.className, checkboxProps }}
+      indicatorSlot={<UCheckboxButtonIndicator />}
+    />
+  );
+};
